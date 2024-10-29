d0688d5b4ced2f45471d5761bd1de343720fc99e
Amir Abbas <shaikh4851@gmail.com>
2024-10-29 12:56:45 +0500

    Create new template user-profile

--- static/js/script.js
+++ static/js/script.js
@@ -100,3 +100,48 @@ $(document).ready(function () {
     $("aside").toggleClass("hidden block");
   });
 });
+
+// Settings.html
+$(document).ready(function () {
+  $("#btn-sidebar-toggle").on("click", () => {
+    $("#sidebar").toggleClass("hidden");
+    $("#btn-sidebar-toggle").toggleClass("ri-menu-fill ri-close-large-line");
+  });
+});
+
+$(document).ready(function () {
+  const $profileTab = $("#my-profile-settings");
+  const $loginDetailsTab = $("#login-details-settings");
+  const $notificationTab = $("#notifications-settings");
+  const $btnProfileTab = $("#btn-my-profile");
+  const $btnLoginDetailsTab = $("#btn-login-details");
+  const $btnNotificationTab = $("#btn-notifications");
+
+  const hiddenClass = "hidden";
+  const activeClass = "active";
+
+  function toggleActiveTab(showTab, ...hideTabs) {
+    showTab.removeClass(hiddenClass);
+    hideTabs.forEach((tab) => tab.addClass(hiddenClass));
+  }
+
+  function setActiveButton(activeButton, ...inactiveButtons) {
+    activeButton.addClass(activeClass);
+    inactiveButtons.forEach((button) => button.removeClass(activeClass));
+  }
+
+  $btnProfileTab.on("click", () => {
+    toggleActiveTab($profileTab, $loginDetailsTab, $notificationTab);
+    setActiveButton($btnProfileTab, $btnLoginDetailsTab, $btnNotificationTab);
+  });
+
+  $btnLoginDetailsTab.on("click", () => {
+    toggleActiveTab($loginDetailsTab, $profileTab, $notificationTab);
+    setActiveButton($btnLoginDetailsTab, $btnProfileTab, $btnNotificationTab);
+  });
+
+  $btnNotificationTab.on("click", () => {
+    toggleActiveTab($notificationTab, $profileTab, $loginDetailsTab);
+    setActiveButton($btnNotificationTab, $btnProfileTab, $btnLoginDetailsTab);
+  });
+});
